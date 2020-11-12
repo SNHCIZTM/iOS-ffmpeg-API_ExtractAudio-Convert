edc895bf8b78c2600bfda28c00c333b14a4b1b2f
gaoxiaoyan <gaoxiaoyan@bilibili.com>
2020-11-12 19:00:03 +0800

    refine logic

--- AudioConverter/ViewController.m
+++ AudioConverter/ViewController.m
@@ -25,12 +25,13 @@
 
 -(void)extractAudio {
     int err_code;
+    char errors[1024];
 
     const char *src_filename = NULL;
-    char *dst_filename = "/Users/gxy/Desktop/ios/audio.aac";
+    char *dst_filename = "/Users/gaoxiaoyan/Desktop/gxy/audio.aac";
     NSString *fromFile = [[NSBundle mainBundle]pathForResource:@"video.mov" ofType:nil];
     src_filename = [fromFile UTF8String];
-
+    
     FILE *dst_fd = NULL;
 
     int audio_stream_index = -1;
@@ -46,12 +47,26 @@
 
     av_log_set_level(AV_LOG_DEBUG);
 
-    if((err_code = avformat_open_input(&fmt_ctx, src_filename, NULL, NULL)) < 0){
-        av_log(NULL, AV_LOG_DEBUG, "Could not open source file\n");
+    if(src_filename == NULL || dst_filename == NULL){
+        av_log(NULL, AV_LOG_DEBUG, "src or dts file is null, plz check them!\n");
     }
 
+    /*open input media file, and allocate format context*/
+    if((err_code = avformat_open_input(&fmt_ctx, src_filename, NULL, NULL)) < 0){
+        av_strerror(err_code, errors, 1024);
+        av_log(NULL, AV_LOG_DEBUG, "Could not open source file: %s, %d(%s)\n",
+               src_filename,
+               err_code,
+               errors);
+    }
+
+    /*retrieve audio stream*/
     if((err_code = avformat_find_stream_info(fmt_ctx, NULL)) < 0) {
-        av_log(NULL, AV_LOG_DEBUG, "failed to find stream information\n");
+        av_strerror(err_code, errors, 1024);
+        av_log(NULL, AV_LOG_DEBUG, "failed to find stream information: %s, %d(%s)\n",
+               src_filename,
+               err_code,
+               errors);
     }
 
     av_dump_format(fmt_ctx, 0, src_filename, 0);
@@ -60,16 +75,46 @@
     AVCodecParameters *in_codecpar = in_stream->codecpar;
     if(in_codecpar->codec_type != AVMEDIA_TYPE_AUDIO){
         av_log(NULL, AV_LOG_ERROR, "The Codec type is invalid!\n");
+        exit(1);
     }
 
     ofmt_ctx = avformat_alloc_context();
     output_fmt = av_guess_format(NULL, dst_filename, NULL);
+    if(!output_fmt){
+        av_log(NULL, AV_LOG_DEBUG, "Cloud not guess file format \n");
+        exit(1);
+    }
+
     ofmt_ctx->oformat = output_fmt;
+
     out_stream = avformat_new_stream(ofmt_ctx, NULL);
+    if(!out_stream){
+        av_log(NULL, AV_LOG_DEBUG, "Failed to create out stream!\n");
+        exit(1);
+    }
+
+    if(fmt_ctx->nb_streams<2){
+        av_log(NULL, AV_LOG_ERROR, "the number of stream is too less!\n");
+        exit(1);
+    }
+
+
+    if((err_code = avcodec_parameters_copy(out_stream->codecpar, in_codecpar)) < 0 ){
+        av_strerror(err_code, errors, ERROR_STR_SIZE);
+        av_log(NULL, AV_LOG_ERROR,
+               "Failed to copy codec parameter, %d(%s)\n",
+               err_code, errors);
+    }
+
     out_stream->codecpar->codec_tag = 0;
 
     if((err_code = avio_open(&ofmt_ctx->pb, dst_filename, AVIO_FLAG_WRITE)) < 0) {
-        av_log(NULL, AV_LOG_DEBUG, "Could not open file\n");
+        av_strerror(err_code, errors, 1024);
+        av_log(NULL, AV_LOG_DEBUG, "Could not open file %s, %d(%s)\n",
+               dst_filename,
+               err_code,
+               errors);
+        exit(1);
     }
 
     av_dump_format(ofmt_ctx, 0, dst_filename, 1);
@@ -80,13 +125,17 @@
 
     audio_stream_index = av_find_best_stream(fmt_ctx, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0);
     if(audio_stream_index < 0){
-        av_log(NULL, AV_LOG_DEBUG, "Could not find stream in input file\n");
+        av_log(NULL, AV_LOG_DEBUG, "Could not find %s stream in input file %s\n",
+               av_get_media_type_string(AVMEDIA_TYPE_AUDIO),
+               src_filename);
     }
 
     if (avformat_write_header(ofmt_ctx, NULL) < 0) {
-        av_log(NULL, AV_LOG_DEBUG, "Error occurred when opening output file\n");
+        av_log(NULL, AV_LOG_DEBUG, "Error occurred when opening output file");
+        exit(1);
     }
 
+    /*read frames from media file*/
     while(av_read_frame(fmt_ctx, &pkt) >=0 ){
         if(pkt.stream_index == audio_stream_index){
             pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, (AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
